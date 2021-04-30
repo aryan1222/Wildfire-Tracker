3c571c2671ec851913c44a3f827b12dfbb12b748
aryan1222 <chhabraaryan12@gmail.com>
2021-04-30 16:20:40 +0530

    fetch data from EONET

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import Map from './components/Map';
+import {useEffect, useState} from 'react';
 
 function App() {
+
+  const [eventData, setEventData] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const res = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+      const {events} = await res.json();
+      //console.log(events);
+
+      setEventData(events);
+    }
+
+    fetchEvents()
+    console.log(eventData)
+  }, [])
+
   return (
     <div>
-        <Map/>
+        <Map eventData={eventData} />
     </div>
   );
 }
